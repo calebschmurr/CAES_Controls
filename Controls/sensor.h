a60715e635597fac900554ee9ec28b03bacaf602
samuelkcrow <42583985+samuelkcrow@users.noreply.github.com>
2021-03-08 14:18:16 -0500

    Moving SSRelay.cpp and cleaning sensor.h

--- Controls/sensor.h
+++ Controls/sensor.h
@@ -2,15 +2,13 @@
 //Used to define the format for the rest of the sensors
 
 
-class Sensor{
+class Sensor {
 
 private:
 
-int pin;
-
-float multiplier;
+    int pin;
+    float multiplier;
 
 public:
-virtual float getValue();//Virtual makes a class abstract.
-
+    virtual float getValue(); //Virtual makes a class abstract.
 };
